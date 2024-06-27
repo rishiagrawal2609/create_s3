16aa05c12bb62d1d0e3c99491273d61286bc723d
rishiagrawal2609 <rishiagrawal2609@gmail.com>
2024-06-27 19:13:53 +0530

    add ec2

--- main.tf
+++ main.tf
@@ -4,3 +4,11 @@ resource "aws_s3_bucket" "my_bucket" {
     bucket = "my-tf-test-bucket-hwefuhlksghlrisdjwfuhe"
 }
 
+resource "aws_instance" "example_server" {
+  ami           = "ami-01b799c439fd5516a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
+}
